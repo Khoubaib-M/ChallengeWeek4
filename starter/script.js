8272caa682ee9397de0cf464e35db9b6af44975b
khoubaib <khoubaib15@outlook.com>
2022-11-15 20:58:01 +0000

    added calculations for greatest increase and decrease in profits

--- starter/script.js
+++ starter/script.js
@@ -8,15 +8,31 @@ for (var i = 0; i < finances.length; i++) {
 }
 
 var changeMonth = [];
-var changeTotal = [];
-var change = 0;
+var change = [];
+var changeTotal = 0;
 
 for (var i = 1; i < finances.length; i++) {
-    var changeMonth = finances[i][1] - finances[i-1][1]
-    change.push(changeMonth)
-    changeMonth.push([finances[i][0], changeMonth])
-    changeTotal += changeMonth
+    var changeEachMonth = finances[i][1] - finances[i-1][1]
+    change.push(changeEachMonth)
+    changeMonth.push([finances[i][0], changeEachMonth])
+    changeTotal += changeEachMonth
 }
 
-var averageChange = (changeTotal/(totalMonths - 1)).toFixed(3)
+var averageChange = (changeTotal/(totalMonths - 1)).toFixed(3);
 
+var greatestIncreaseIndex = change.indexOf(Math.min(...change));
+greatestIncrease = changeMonth[greatestIncreaseIndex];
+
+var greatestDecreaseIndex = change.indexOf(Math.min(...change));
+greatestDecrease = changeMonth[greatestDecreaseIndex];
+
+var analysis = 
+`Financial Analysis
+----------------------------
+Total Months: $${totalMonths}
+Total: $${netProfit}
+Average Change: $${averageChange}
+Greatest Increase in Profits: ${greatestIncrease[0]} ($${greatestIncrease[1]})
+Greatest Decrease in Profits: ${greatestDecrease[0]} ($${greatestDecrease[1]})`
+
+console.log(analysis)
